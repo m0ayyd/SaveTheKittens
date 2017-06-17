c4c0be965d489c58d5bf33219d77179f44ef8e2c
the Luckiest <m02yyd@gmail.com>
2017-06-17 03:41:17 +0300

    coder A print

--- SaveTheKittens/ViewController.swift
+++ SaveTheKittens/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Coder A Coding")
     }
 
     override func didReceiveMemoryWarning() {
